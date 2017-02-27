e9b8cec571feec5d1d3c41aa5399226011bfca5d
Brett Beaulieu-Jones <brettbj@gmail.com>
2017-02-26 19:48:56 -0500

    name

--- 5_h5py_to_csv.py
+++ 5_h5py_to_csv.py
@@ -7,8 +7,9 @@ parser.add_argument("--name", type=str)
 args = parser.parse_args()
 
 args.name = args.name.split('/')[-1]
-print(args.name)
-f = h5py.File('./data/spikein/' + args.name, 'r')
-X = f['dataset'][:]
-np.savetxt('./data/spikeincsv/' + args.name + '.csv',
-           X, delimiter=',', newline='\n')
+if '.p' not in args.name:
+    print(args.name)
+    f = h5py.File('./data/spikein/' + args.name, 'r')
+    X = f['dataset'][:]
+    np.savetxt('./data/spikeincsv/' + args.name + '.csv',
+               X, delimiter=',', newline='\n')
