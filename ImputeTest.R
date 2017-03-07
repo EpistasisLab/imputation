bc699ddd3fc3c13a9d90f533d3fb16ea790b5a03
Brett Beaulieu-Jones <brettbj@gmail.com>
2017-03-07 08:35:29 -0500

    Figure 2 Combined

--- ImputeTest.R
+++ ImputeTest.R
@@ -6,44 +6,40 @@ args = commandArgs(trailingOnly=TRUE)
 setwd('./data/')
 print(args[1])
 file_name = paste(args[1], '.csv', sep='')
-X_missing = read.csv(file_name)
+X_missing = read.csv(file_name)[1:9999,]
 print(dim(X_missing))
 set.seed(23515)
 #print(head(X_missing))
 
 tic<-Sys.time()
-#impMethod<-c("pmm", "norm", "norm.nob", "norm.boot", "norm.predict", "mean", "rf", "ri", "sample") # quadratic throws warnings glm.fit: algorithm did not converge 
-#impMethod<-c("pmm", "norm") # quadratic throws warnings glm.fit: algorithm did not converge 
+#impMethod<-c("pmm", "norm", "norm.nob", "norm.boot", "norm.predict", "mean", "rf", "ri", "sample") # quadratic throws warn$
+impMethod<-c("pmm", "norm") # quadratic throws warnings glm.fit: algorithm did not converge
 impList<-list()
 ini <- mice(X_missing, maxit = 0)
 
 eval_list<-list()
 X = read.csv('./completeCasesBoxCox.csv')[1:9999,2:32]
-print(dim(X))
-
-X = X[1:250,]
-X_missing = X_missing[1:250,]
-
 X.df <- data.frame(matrix(unlist(X)))
 
-print(args[2])
-imp <- mice(X_missing, visitSequence = "monotone", predictorMatrix = ini$predictorMatrix, 
-            ridge=.3, m=1, method = args[2], maxit = 100)
-
-impValues<-imp$imp
-measures<-X_missing
-print(dim(impValues))
-print(dim(measures))
-for (j in 1:length(impValues)){
-  measures[which(is.na(measures[,j])),j]<-impValues[[j]][,1]
+for(i in 1:length(impMethod)){
+  print(impMethod[i])
+  imp <- mice(X_missing, visitSequence = "monotone", predictorMatrix = ini$predictorMatrix, ridge=.3,
+              m=1, method = impMethod[i], maxit = 1)
+  impList[[i]]<-imp
+  
+  splt<-imp$method[1]
+  impValues<-impList[[i]]$imp
+  measures<-X_missing
+  for (j in 1:length(impValues)){
+    measures[which(is.na(measures[,j])),j]<-impValues[[j]][,1]
+  }
+  m.df <- data.frame(matrix(unlist(measures)))
+  eval_list[i] = sqrt(mean((m.df-X.df)^2))
 }
-m.df <- data.frame(matrix(unlist(measures)))
-eval_list = sqrt(mean((m.df-X.df)^2))
-
 Sys.time()-tic
 
-file_name <- paste('../output/R', args[1], args[2], '.csv', sep='_')
+file_name <- paste('../output/R', args[1], sep='_')
 print(file_name)
 eval.df <- data.frame(matrix(unlist(eval_list)))
-rownames(eval.df) <- args[2]
+rownames(eval.df) <- impMethod
 write.csv(eval.df, file=file_name)
--- figure_2.r
+++ figure_2.r
@@ -11,10 +11,25 @@ print(dim(mcar_scores))
 mcar_scores <- mcar_scores[, !(names(mcar_scores) %in% "X")]
 colnames(mcar_scores) <- c("10%", "20%", "30%", "40%", "50%")
 mcar_df <- melt(mcar_scores)
-colnames(mcar_df) <- c("Spikein", "ROCAUC")
+colnames(mcar_df) <- c("Spikein", "ROC_AUC")
 
-ggplot(mcar_df, aes(x=Spikein, y=ROC_AUC, color=Spikein)) +
-  geom_boxplot(outlier.colour="black", outlier.shape=1, outlier.size=1)
+png("./plots/figure_2a.png", width = 12, height = 7, res = 300, units = "in")
+  print(ggplot(mcar_df, aes(x=Spikein, y=ROC_AUC, color=Spikein)) +
+    geom_boxplot(outlier.colour="black", outlier.shape=1, outlier.size=1) +
+    labs(title = "Missing Completely at Random Simulation Missingess Prediction", y="ROC AUC", x="Spike-in Percentage")
+  )
+dev.off()
+
+twoA <- ggplot(mcar_df, aes(x=Spikein, y=ROC_AUC, color=Spikein)) +
+  geom_boxplot(outlier.colour="black", outlier.shape=1, outlier.size=1) +
+  labs(title = "Missing Completely at Random Simulation Missingess Prediction", y="ROC AUC", x="Spike-in Percentage")
+
+png("./plots/supp_figure_2a.png", width = 12, height = 7, res = 300, units = "in")
+print(ggplot(mcar_df, aes(x=Spikein, y=ROC_AUC, color=Spikein)) +
+        geom_violin() +
+        labs(title = "Missing Completely at Random Simulation Missingess", y="ROC AUC", x="Spike-in Percentage")
+)
+dev.off()
 
 # MAR
 mar_scores<-read.csv('./data/prediction_scores/mar.csv')
@@ -25,8 +40,23 @@ colnames(mar_scores) <- c("Quartile 1", "Quartile 2", "Quartile 3", "Quartile 4"
 mar_df <- melt(mar_scores)
 colnames(mar_df) <- c("Quartile", "ROC_AUC")
 
-ggplot(mar_df, aes(x=Quartile, y=ROC_AUC, color=Quartile)) +
-  geom_boxplot(outlier.colour="black", outlier.shape=1, outlier.size=1)
+png("./plots/figure_2b.png", width = 12, height = 7, res = 300, units = "in")
+print(ggplot(mar_df, aes(x=Quartile, y=ROC_AUC, color=Quartile)) +
+  geom_boxplot(outlier.colour="black", outlier.shape=1, outlier.size=1) +
+  labs(title="Missing At Random Simulation Missingness Prediction", y="ROC AUC", x="Quartile")
+)
+dev.off()
+
+twoB <- ggplot(mar_df, aes(x=Quartile, y=ROC_AUC, color=Quartile)) +
+  geom_boxplot(outlier.colour="black", outlier.shape=1, outlier.size=1) +
+  labs(title="Missing At Random Simulation Missingness Prediction", y="ROC AUC", x="Quartile")
+
+png("./plots/supp_figure_2b.png", width = 12, height = 7, res = 300, units = "in")
+print(ggplot(mar_df, aes(x=Quartile, y=ROC_AUC, color=Quartile)) +
+  geom_violin() +
+  labs(title="Missing Not at Random Simulation Missingness Prediction", y="ROC AUC", x="Quartile")
+)
+dev.off()
 
 # MNAR 
 mnar_scores<-read.csv('./data/prediction_scores/mnar.csv')
@@ -37,25 +67,67 @@ colnames(mnar_scores) <- c("Quartile 1", "Quartile 2", "Quartile 3", "Quartile 4
 mnar_df <- melt(mnar_scores)
 colnames(mnar_df) <- c("Quartile", "ROC_AUC")
 
-ggplot(mnar_df, aes(x=Quartile, y=ROC_AUC, color=Quartile)) +
-  geom_boxplot(outlier.colour="black", outlier.shape=1, outlier.size=1)
+png("./plots/figure_2c.png", width = 12, height = 7, res = 300, units = "in")
+print(ggplot(mnar_df, aes(x=Quartile, y=ROC_AUC, color=Quartile)) +
+  geom_boxplot(outlier.colour="black", outlier.shape=1, outlier.size=1) + 
+  labs(title="Missing Not at Random Simulation Missingness Prediction", y="ROC AUC", x="Quartile")
+) 
+dev.off()
+
+twoC <- ggplot(mnar_df, aes(x=Quartile, y=ROC_AUC, color=Quartile)) +
+  geom_boxplot(outlier.colour="black", outlier.shape=1, outlier.size=1) + 
+  labs(title="Missing Not at Random Simulation Missingness Prediction", y="ROC AUC", x="Quartile")
+
+png("./plots/supp_figure_2c.png", width = 12, height = 7, res = 300, units = "in")
+print(ggplot(mnar_df, aes(x=Quartile, y=ROC_AUC, color=Quartile)) +
+        geom_violin() +
+        labs(title="Missing Not at Random Simulation Missingness Prediction", y="ROC AUC", x="Quartile")
+)
+dev.off()
 
 # all scores
 scores<-read.csv("./data/prediction_scores/real_all_labs.csv")
 print(dim(scores))
 
 drops <- c("X")
-scores[ , !(names(scores) %in% drops)]
-boxplot(scores)
+scores[ , !(colnames(scores) %in% drops)]
 
+#2d
 scores_df <- melt(scores)
 colnames(scores_df) <- c("Lab", "ROC_AUC")
-ggplot(scores_df, aes(x=Lab, y=ROC_AUC, color=Lab)) +
+
+png("./plots/figure_2d.png", width = 12, height = 7, res = 300, units = "in")
+print(ggplot(scores_df, aes(x=Lab, y=ROC_AUC, color=Lab)) +
   geom_boxplot(outlier.colour="black", outlier.shape=1, outlier.size=1)
+)
+
+#2e
+png("./plots/figure_2e.png", width = 12, height = 7, res = 300, units = "in")
+print(ggplot(scores_df, aes(x=Lab, y=ROC_AUC, color=Lab)) +
+      geom_violin()
+)
 
 # top 28
 select_scores<-read.csv("./data/prediction_scores/real28.csv")
 print(dim(select_scores))
 
 select_scores <- select_scores[, !(names(select_scores) %in% "X")]
-boxplot(select_scores, horizontal=FALSE)
+select_scores_df <- melt(select_scores)
+colnames(select_scores_df) <- c("Lab", "ROC_AUC")
+
+#2f
+png("./plots/figure_2f.png", width = 12, height = 7, res = 300, units = "in")
+print(ggplot(select_scores_df, aes(x=Lab, y=ROC_AUC, color=Lab)) +
+        geom_boxplot(outlier.colour="black", outlier.shape=1, outlier.size=1) + 
+        labs(title="Most Complete 28 Labs Missingness Prediction", y="ROC AUC", x="Lab")
+) 
+dev.off()
+
+#2g
+png("./plots/figure_2g.png", width = 12, height = 7, res = 300, units = "in")
+print(ggplot(select_scores_df, aes(x=Lab, y=ROC_AUC, color=Lab)) +
+        geom_violin() + 
+        labs(title="Most Complete 28 Labs", y="ROC AUC", x="Lab")
+) 
+dev.off()
+
