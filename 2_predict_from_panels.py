c8244533cff79f0d9b9c700b0fdd84a27ad7c3e5
Brett <brettbj@gmail.com>
2017-06-21 16:21:41 -0400

    update for running

--- 2_predict_from_panels.py
+++ 2_predict_from_panels.py
@@ -45,35 +45,22 @@ if __name__ == "__main__":
     X = pd.read_csv('./data/all_patients_all_labs.csv')
     print(X.columns)
 
-    panels = pd.read_csv('./data/labPanelMembershipMatrix.txt', delimiter='\t')
-    panels.set_index("NA", inplace=True)
+    lab_matrix = pd.read_csv('./data/labPanelMembershipMatrix.txt',
+                             delimiter='\t')
+    lab_matrix.set_index("NA", inplace=True)
 
     all_patients_scores = {}
 
     # only keep the first value in each panel
-    panel_list = ["751-8", "1743-4", "10466-1", "20565-8", "2708-6", "1925-7",
-                  "19994-3", "1990-1", "2157-6", "12187-1", "6303-2", "3181-5",
-                  "27811-9", "27818-4", "27822-6", "13982-4", "2064-4",
-                  "3167-4", "2888-6", "28009-9", "2132-9", "2284-8", "2965-2",
-                  "18262-6", "2857-1", "30934-4", "33762-6", "10535-3",
-                  "3026-2", "1834-1", "21198-7", "3024-7", "3051-0", "11572-5",
-                  "29958-6", "13964-2", "13965-9", "30522-7", "1763-2",
-                  "2143-6", "5206-8", "20448-7", "11580-8", "2742-5", "1986-9",
-                  "1798-8", "20563-3", "17862-4", "1825-9", "10330-9",
-                  "23860-0", "2340-8", "3255-7", "17849-1", "1988-5",
-                  "10886-0", "20437-0", "13967-5", "2986-8", "2991-8",
-                  "10501-5", "20433-9", "2243-4", "2571-8", "14957-5",
-                  "17856-6", "27353-2", "72586-1", "2465-3", "2458-8", "763-3",
-                  "740-1", "2731-8", "2276-4", "2502-3", "53115-2", "10334-1",
-                  "2039-6", "1968-7", "14338-8", "19123-9", "2324-2", "2777-1",
-                  "12180-6", "14804-9", "3084-1"]
 
-    for p in panel_list:
-        cols = panels.loc[panels[p] == 1].index
+    for p in lab_matrix:
+        print(p)
+
+        cols = lab_matrix.loc[lab_matrix[p] == 1].index
         cols = cols.drop(p)
 
         X_no_panel = X.drop(cols, axis=1)
         all_patients_scores[p] = run_predict('all_patients', X_no_panel, p)
 
-        pkl.dump(all_patients_scores, open('data/all_patients/' +
+        pkl.dump(all_patients_scores, open('data/all_patients_' +
                                            'all_panel_scores.p', 'w+'))
